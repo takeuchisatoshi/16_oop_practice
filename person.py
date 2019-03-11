d4c2880e9d094f2de5b65d98fe6dcf3dc396b8e7
takeuchisatoshi <satsitt3@gmail.com>
2019-03-11 09:09:47 +0900

    Ken を追加

--- person.py
+++ person.py
@@ -4,14 +4,16 @@ class Person:
         self.age = age
 
     def profile(self):
-        return f"Name: {self.name}, {self.age}"
+        return f"Name: {self.name}, Age: {self.age}"
 
 
 if __name__ == '__main__':
     # 'クラス'を使うイメージ
     bob = Person("Bob", 15)
-
     print(bob.profile())  # 'Name: Bob, Age: 15' と出力されてほしい
 
     tom = Person("Tom", 24)
     print(tom.profile())  # 'Name: Tom, Age: 24' と出力されてほしい
+
+    ken = Person("Ken", 42)
+    print(ken.profile())  # 'Name: Ken, Age: 42' と出力されてほしい
