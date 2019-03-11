4ec6e27450e2af43810b99cbba85a23465deafc9
takeuchisatoshi <satsitt3@gmail.com>
2019-03-11 09:04:49 +0900

    Person クラスを実装

---
+++ person.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def profile(self):
+        return f"Name: {self.name}, {self.age}"
+
+
+if __name__ == '__main__':
+    # 'クラス'を使うイメージ
+    bob = Person("Bob", 15)
+
+    print(bob.profile())  # 'Name: Bob, Age: 15' と出力されてほしい
+
+    tom = Person("Tom", 24)
+    print(tom.profile())  # 'Name: Tom, Age: 24' と出力されてほしい
