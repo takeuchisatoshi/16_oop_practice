048d25a38928915d3e65484ebd6ce87828ba083c
takeuchisatoshi <satsitt3@gmail.com>
2019-03-11 11:57:07 +0900

    メソッドを追加

---
+++ circle.py
@@ -0,0 +1,24 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return (self.radius ** 2) * 3.14
+
+    def circumference(self):
+        return 2 * self.radius * 3.14
+
+
+if __name__ == '__main__':
+    circle = Circle(radius=3)
+    print(circle.area())
+    print(circle.circumference())
+
+    circle2 = Circle(20)
+    print(circle2.area())
+    print(circle2.circumference())
+
+    input_number = int(input("数字を入力"))
+    circle3 = Circle(input_number)
+    print(circle3.area())
+    print(circle3.circumference())
--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,19 @@
 class Rectangle:
-    def __init__(self):
-        print(f"Called! __init__")
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
 
 
 if __name__ == '__main__':
-    r1 = Rectangle()
+    r1 = Rectangle(4, 3)
+    # 3分課題
+    area1 = r1.area() #面積を計算する
+    print(area1) # 12が出力される
 
-    r2 = Rectangle()
+    r2 = Rectangle(10, 5)
+    area2 = r2.area()
+    print(area2)
+
